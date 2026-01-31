9cbe9bb89f84f7762e9a24f9d26574644bd54a5e
imshaaaa <kyle13mod@gmail.com>
2026-01-31 23:28:30 +0800

    initial commit

---
+++ profile.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <style>
+      img {
+        height: 150px;
+        width: 150px;
+        border-radius: 100%;
+        
+      }
+      body {
+        margin: 0;        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;    
+        min-height: 100vh;
+        text-align: center;
+      }
+    </style>
+</head>
+<body style="align-content: center">
+  <img src="img.jpg" style="display: block" />
+  <h1>Shallom Kyle Jacinto</h1>
+  <h1>BSIT</h1>
+  <h1>24 years old</h1>
+  <h1>Lapu-lapu city</h1>
+</body>
+</html>
